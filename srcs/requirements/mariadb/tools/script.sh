5bb6a12e4b3b445531ac0c1c57249969c4362032
anaraujo <andreiacampos98.15@gmail.com>
2023-10-07 17:17:34 +0100

    wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,26 +4,26 @@ mysql_install_db;
 service mysql start;
 
 # Configure database
-if [ -f /var/lib/mysql/${DB_NAME} ]
-	echo "${DB_NAME} already created"
+if [ -f /var/lib/mysql/${db_name} ]
+	echo "${db_name} already created"
 then
 	# creating database
-	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${db_name};"
 	# creating user DB_USER %
-	mariadb -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-	mariadb -u root -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+	mariadb -u root -e "CREATE USER IF NOT EXISTS '${db_user}'@'%' IDENTIFIED BY '${db_pwd}';"
+	mariadb -u root -e "GRANT ALL ON ${db_name}.* TO '${db_user}'@'%' IDENTIFIED BY '${db_pwd}';"
 	mariadb -u root -e "FLUSH PRIVILEGES;"
-	# creating user DB_USER local
-	mariadb -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-	mariadb -u root -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	# creating user db_user local
+	mariadb -u root -e "CREATE USER IF NOT EXISTS '${db_user}'@'localhost' IDENTIFIED BY '${db_pwd}';"
+	mariadb -u root -e "GRANT ALL ON ${db_name}.* TO '${db_user}'@'localhost' IDENTIFIED BY '${db_pwd}';"
 	mariadb -u root -e "FLUSH PRIVILEGES;"
-	echo "${DB_NAME} created database"
+	echo "${db_name} created database"
 	
-	mariadb -u root -e "CREATE TABLE ${DB_NAME}.todo_list (item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id));"
-	mariadb -u root -e "INSERT INTO ${DB_NAME}.todo_list (content) VALUES ('My first task, yay');"
-	mariadb -u root -e "INSERT INTO ${DB_NAME}.todo_list (content) VALUES ('Second task, gimme more');"
-	mariadb -u root -e "INSERT INTO ${DB_NAME}.todo_list (content) VALUES ('Last task i promise');"
-	mariadb -u root -e "INSERT INTO ${DB_NAME}.todo_list (content) VALUES ('one more task');"
+	mariadb -u root -e "CREATE TABLE ${db_name}.todo_list (item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id));"
+	mariadb -u root -e "INSERT INTO ${db_name}.todo_list (content) VALUES ('My first task, yay');"
+	mariadb -u root -e "INSERT INTO ${db_name}.todo_list (content) VALUES ('Second task, gimme more');"
+	mariadb -u root -e "INSERT INTO ${db_name}.todo_list (content) VALUES ('Last task i promise');"
+	mariadb -u root -e "INSERT INTO ${db_name}.todo_list (content) VALUES ('one more task');"
 fi
 
 service mysql stop;
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,22 +1,14 @@
-#!/bin/bash
-
-sleep 10;
-# while ! nc -z "mariadb" "3306"; do
-# 	sleep 1;
-# done
-
-# if [ -f "/var/www/html/wp-config.php" ]; then
-# 	echo "${WP_URL} already created"
-# else
-	cp -f /tmp/wp-config.php /var/www/html/
-	chmod 644 /var/www/html/wp-config.php;
-	# create administrator
-	wp core install --url="anaraujo.42.fr" --title="inception" --admin_name="anaraujo --admin_email="andreiacampos98.15@gmail.com --admin_password="12345" --allow-root
-	# Create user
-	wp user create "andreia" "anaraujo@student.42porto.com" --user_pass="1234" --allow-root
-# fi
-
-service php7.3-fpm start;
-service php7.3-fpm stop;
-
-php-fpm7.3 -F -R;
+wp core install --url="$DOMAIN_NAME"\
+				--title="$WP_TITLE" \
+				--admin_user="$WP_ADMIN_USR" \
+				--admin_password="$WP_ADMIN_PWD" \
+				--admin_email="$WP_ADMIN_EMAIL" \
+				--skip-email \
+				--path='/var/www/html' \
+				--allow-root
+wp user create "$WP_USER" "$WP_EMAIL" \
+				--user_pass="$WP_PWD" \
+				--role=author \
+				--path='/var/www/html' \
+				--allow-root
+/usr/sbin/php-fpm7.4 -F
